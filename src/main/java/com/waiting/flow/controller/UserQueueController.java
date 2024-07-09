5730745eee059b70382c51c1655d35993b76413e
minutaurus <made_power@naver.com>
2024-07-09 22:40:32 +0900

    Controller 생성
    UserQueue와 Id를 인자로 받는다.

---
+++ src/main/java/com/waiting/flow/controller/UserQueueController.java
@@ -0,0 +1,26 @@
+package com.waiting.flow.controller;
+
+import com.waiting.flow.dto.RegisterUserResponse;
+import com.waiting.flow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/queue")
+@RequiredArgsConstructor
+public class UserQueueController {
+
+    private final UserQueueService userQueueService;
+
+    @PostMapping("")
+    public Mono<RegisterUserResponse> registerUser( @RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                    @RequestParam(name = "user_id") Long userId){
+        return  userQueueService.registerWaitQueue(queue,userId)
+                .map(RegisterUserResponse::new);
+    }
+
+}
