840fdab8c0a597d2e458eb8e6d2c0bc3d47978f2
minutaurus <made_power@naver.com>
2024-07-10 14:34:05 +0900

    대기열 진입 및 진입 가능여부 조회 로직
    Redis에 WaitQueue를 조회하여 대기열에 진입가능한지 조회를 하고 진입이 가능하다면 popMin을 통해 WaitQueue의 맨 앞에 있는 유저를 제거하고 실행 가능한 ProceedQueue에 적재하는 로직이다.

--- src/main/java/com/waiting/flow/service/UserQueueService.java
+++ src/main/java/com/waiting/flow/service/UserQueueService.java
@@ -14,6 +14,7 @@ public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY="user-queue:%s:wait";
+    private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
 
     // 대기열 등록 API
@@ -28,6 +29,24 @@ public class UserQueueService {
                 .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERED_USER.build())) // False일 때 Error
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())) //
                 .map(i -> i >= 0  ?  i + 1: i );
+    }
+
+    // 진입을 허용
+    public Mono<?> allowUser(final String queue, final Long count){
+        // 진입을 허용하는 단계
+        // 1.wait queue 사용자를 제거
+        // 2. proceed queue 사용자를 추가
+        return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue),count)
+                .flatMap( member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond() ))
+                .count();
+    }
+
+
+    // 진입이 가능한 상태인지 조회
+    public Mono<Boolean> isAllowed(final String queue, final Long userId){
+        return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue),userId.toString())
+                .defaultIfEmpty(-1L)
+                .map(rank -> rank >= 0);
     }
 
 }
