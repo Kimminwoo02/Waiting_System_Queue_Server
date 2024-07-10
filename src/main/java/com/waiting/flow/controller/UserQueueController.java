f31a3c5031acd89c94f3a8d2e1a74abb50f50f14
minutaurus <made_power@naver.com>
2024-07-10 14:34:21 +0900

    대기열 진입 Controller 작성

--- src/main/java/com/waiting/flow/controller/UserQueueController.java
+++ src/main/java/com/waiting/flow/controller/UserQueueController.java
@@ -1,12 +1,11 @@
 package com.waiting.flow.controller;
 
+import com.waiting.flow.dto.AllowUserResponse;
+import com.waiting.flow.dto.AllowedUserResponse;
 import com.waiting.flow.dto.RegisterUserResponse;
 import com.waiting.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -21,6 +20,21 @@ public class UserQueueController {
                                                     @RequestParam(name = "user_id") Long userId){
         return  userQueueService.registerWaitQueue(queue,userId)
                 .map(RegisterUserResponse::new);
+    }
+
+    @PostMapping("/allow")
+    public Mono<AllowUserResponse> allowUser(@RequestParam(name = "queue" , defaultValue = "default") String queue,
+                                             @RequestParam(name = "count") Long count) {
+
+        return userQueueService.allowUser(queue,count)
+                .map(allowed -> new AllowUserResponse(count, (Long) allowed));
+    }
+
+    @GetMapping("/allowed")
+    public Mono<?> isAllowedUser( @RequestParam(name = "queue", defaultValue = "default") String queue,
+                                  @RequestParam(name = "user_id") Long userId){
+        return userQueueService.isAllowed(queue, userId)
+                .map(AllowedUserResponse::new);
     }
 
 }
