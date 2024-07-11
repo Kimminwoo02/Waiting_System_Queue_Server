f88baa76a305e61ec9be2ae4f83f88e72aff2181
minutaurus <made_power@naver.com>
2024-07-11 14:46:09 +0900

    주기적으로 대기열 Queue체크
    대기열을 체크하여 접속을 허가할 수 있도록 로직을 추가

--- src/main/java/com/waiting/flow/service/UserQueueService.java
+++ src/main/java/com/waiting/flow/service/UserQueueService.java
@@ -2,19 +2,28 @@ package com.waiting.flow.service;
 
 import com.waiting.flow.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY="user-queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN="user-queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
 
     // 대기열 등록 API
@@ -53,6 +62,25 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue),userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank+1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser(){
+        if(!scheduling) return;
+
+        log.info("called scheduling");
+
+        long maxAllowUserCount = 3;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                .count(100)
+                .build())
+                .map(key-> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount ).map(allowed -> Tuples.of(queue,allowed))) // Queue의 이름이 2번째 인덱스에 있으므로 2번째 값을 가져온다.
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(),tuple.getT1()) ))
+                .subscribe();
+
     }
 
 }
