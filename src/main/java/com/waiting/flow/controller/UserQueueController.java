5d282b83ca95941a089129141ab73931ad9ccb2f
minutaurus <made_power@naver.com>
2024-07-11 17:51:36 +0900

    Cookie를 사용하여 대기열에 접속할 수 있도록 전략을 변경

--- src/main/java/com/waiting/flow/controller/UserQueueController.java
+++ src/main/java/com/waiting/flow/controller/UserQueueController.java
@@ -6,8 +6,13 @@ import com.waiting.flow.dto.RankNumberResponse;
 import com.waiting.flow.dto.RegisterUserResponse;
 import com.waiting.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/queue")
@@ -43,6 +48,26 @@ public class UserQueueController {
                                                  @RequestParam(name = "user_id") Long userId){
         return userQueueService.getRank(queue,userId)
                 .map(RankNumberResponse::new);
+    }
+
+    @GetMapping("/touch")
+    Mono<?> touch(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                  @RequestParam(name = "user_id")Long userId,
+                  ServerWebExchange exchange) throws NoSuchAlgorithmException {
+
+        return Mono.defer(()-> userQueueService.generateToken(queue,userId))
+                .map(token -> {
+                    exchange.getResponse().addCookie(
+                            ResponseCookie.from("user-queue-%s-token".formatted(queue), token)
+                                    .maxAge(Duration.ofSeconds(300))
+                                    .path("/")
+                                    .build()
+                    );
+
+                    return token;
+                });
+
+
     }
 
 }
